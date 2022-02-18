c9da89258ddc200b76f0496fa49739cd716078db
Juan Ceballos <jacu29@gmail.com>
2022-02-17 22:29:52 -0500

    Stats Preview

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const stats = [
+    { id:0,title: "10k+", description: "companies" },
+    { id:1,title: "314+", description: "templates" },
+    { id:2,title: "12M+", description: "queries" },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="card">
+                <div className="card__header">
+                    <div className="card__header__filter"></div>
+                </div>
+                <div className="card__body">
+                    <div className="card__body__information">
+                        <h1>
+                            Get <span>insights</span> that help your business
+                            grow.
+                        </h1>
+                        <p>
+                            Discover the benefits of data analytics and make
+                            better decisions regarding revenue, customer
+                            experience, and overall efficiency.
+                        </p>
+                    </div>
+                    <div className="card__body__stats">
+                        {stats.map(({ id, title, description }) => (
+                            <div key={id} className="card__stat">
+                                <h2>{title}</h2>
+                                <h4>{description}</h4>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
